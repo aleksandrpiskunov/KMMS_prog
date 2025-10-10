8052e07a8cacab3cc2681e03ea92275f45a5558b
Александр Пискунов <aleksandrpiskunov@MacBook-Air-Aleksandr-2.local>
2025-10-10 23:35:37 +0300

    Bubble_sort compilation

--- 2025_09_20_module_app/io.cpp
+++ 2025_09_20_module_app/io.cpp
@@ -1,7 +1,12 @@
 #include "io.hpp"
-
 #include <iostream>
 
 void biv::print_array(const char* const comment, int* arr, const int size) {
-	// TODO
+    std::cout << comment << " ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i];
+        if (i < size - 1) std::cout << ", ";
+    }
+	
+    std::cout << std::endl;
 }
