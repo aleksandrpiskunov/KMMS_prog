d6236618487ff19de1c34a2d89d0646ef96094ea
Александр Пискунов <aleksandrpiskunov@MacBook-Air-Aleksandr-4.local>
2025-11-19 20:17:31 +0300

    Edit conflicts

--- 2025_09_20_module_app/main.cpp
+++ 2025_09_20_module_app/main.cpp
@@ -1,16 +1,15 @@
-// main.cpp
 #include "io.hpp"
-#include "LIS.hpp"
+#include "sortings.hpp"
 
 int main() {
-    int n;
-    int arr[MAXN];
-    int lisseq[MAXN];
-    int length;
+    int arr[] = {1, 8, 2, 5, 3, 11};
+	const int arr_size = sizeof(arr) / sizeof(arr[0]);
+	
+	pav::print_array("Массив до сортировки:", arr, arr_size);
+    
+    pav::sortings::bubble_sort(arr, arr_size);
 
-    readInput(arr, n);
-    computeLIS(arr, n, lisseq, length);
-    printOutput(lisseq, length);
+    pav::print_array("Массив после сортировки:", arr, arr_size);
 
     return 0;
 }
